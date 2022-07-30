6d41c38992223d2e6322fdbced58cb0fe0693850
Andrey Fits <andreyfits@gmail.com>
2022-07-30 12:05:18 +0300

    feat: implement guessing-game

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,25 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+        this.min = this.max = this.mid = 0;
+    }
 
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
     }
 
     guess() {
-
+        return this.mid = Math.round(this.min + (this.max - this.min) / 2);
     }
 
     lower() {
-
+        this.max = this.mid;
+        this.setRange(this.min, this.max);
     }
 
     greater() {
-
+        this.min = this.mid;
+        this.setRange(this.min, this.max);
     }
 }
 
